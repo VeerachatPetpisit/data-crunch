f86a93db1b6a01ac9c8acfff9509efdba0331a00
VeerachatPetpisit <71434441+VeerachatPetpisit@users.noreply.github.com>
2020-09-17 18:48:34 +0700

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -4,6 +4,8 @@ import numpy as np
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
 st.write("""
 # Penguin Prediction App
 
